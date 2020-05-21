9dbecfda86cf613dd2bf28ce87dc99b1052fb4fb
Jennifer Marsman <jennmar@microsoft.com>
2020-05-21 00:22:49 -0400

    Improved error handling in InvokeHealthEntityExtraction function

--- InvokeHealthEntityExtraction/InvokeHealthEntityExtraction/__init__.py
+++ InvokeHealthEntityExtraction/InvokeHealthEntityExtraction/__init__.py
@@ -4,11 +4,11 @@ import azure.functions as func
 import os
 import pickle
 import requests
+import sys
 
 # Global configuration
 max_sentences = 15
 ta_url = TODO
-
 
 """
 This function accepts json-formatted data with separate fields for a research paper's title, abstract, and body.  
@@ -88,6 +88,7 @@ def transform_value(value, umls_concept_dict):
             "errors": [ { "message": "Error:" + error.args[0] }   ]       
             })
 
+    # Call TA Health API
     try:
         documents = {
             "documents": []
@@ -128,7 +129,19 @@ def transform_value(value, umls_concept_dict):
         json_object = json.dumps(json_response, indent = 4) 
         logging.info("Response from TA: " + json_object)
 
+    except:
+        exc_tuple = sys.exc_info()
+        logging.error(str(exc_tuple))
+        return (
+            {
+            "recordId": recordId,
+            "errors": [ { "message": "Failure during health entity extraction for record " + recordId + ".  e: " + str(exc_tuple)}   ]
+            })
+
+    # Process output into format to be accepted by a custom skill
+    try:
         # Initialize health entities
+        BODY_STRUCTURE = []
         CONDITION_QUALIFIER = []
         DIAGNOSIS = []
         DIRECTION = []
@@ -142,52 +155,54 @@ def transform_value(value, umls_concept_dict):
         ROUTE_OR_MODE = []
         SYMPTOM_OR_SIGN = []
         TREATMENT_NAME = []
-        BODY_STRUCTURE = []
         VARIANT = []
         logging.info("Initialized health entities")
 
+        #if "documents" not in json_response:
+        #    logging.warn("No documents: " + json_object)
+
         # Put the output of the Text Analytics Entity Extraction in the interface for a custom skill output
         for doc in json_response["documents"]:
-            logging.info("Document: " + json.dumps(doc))
             for ent in doc["entities"]:
-                #logging.info("Entity: " + json.dumps(ent))
                 if "links" in ent:
                     for link in ent["links"]:
                         if link["dataSource"] == "UMLS":
-                            if ent['type'] == 'TREATMENT_NAME':
-                                TREATMENT_NAME.append(umls_concept_dict[link['id']])
-                            elif ent['type'] == 'BODY_STRUCTURE':
-                                BODY_STRUCTURE.append(umls_concept_dict[link['id']])
-                            if ent['type'] == 'CONDITION_QUALIFIER':
-                                CONDITION_QUALIFIER.append(umls_concept_dict[link['id']])
-                            elif ent['type'] == 'DIAGNOSIS':
-                                DIAGNOSIS.append(umls_concept_dict[link['id']])
-                            elif ent['type'] == 'DIRECTION':
-                                DIRECTION.append(umls_concept_dict[link['id']])
-                            elif ent['type'] == 'EXAMINATION_NAME':
-                                EXAMINATION_NAME.append(umls_concept_dict[link['id']])
-                            elif ent['type'] == 'EXAMINATION_RELATION':
-                                EXAMINATION_RELATION.append(umls_concept_dict[link['id']])
-                            elif ent['type'] == 'FAMILY_RELATION':
-                                FAMILY_RELATION.append(umls_concept_dict[link['id']])
-                            elif ent['type'] == 'GENDER':
-                                GENDER.append(umls_concept_dict[link['id']])
-                            elif ent['type'] == 'GENE':
-                                GENE.append(umls_concept_dict[link['id']])
-                            elif ent['type'] == 'MEDICATION_CLASS':
-                                MEDICATION_CLASS.append(umls_concept_dict[link['id']])
-                            elif ent['type'] == 'MEDICATION_NAME':
-                                MEDICATION_NAME.append(umls_concept_dict[link['id']])
-                            elif ent['type'] == 'ROUTE_OR_MODE':
-                                ROUTE_OR_MODE.append(umls_concept_dict[link['id']])
-                            elif ent['type'] == 'SYMPTOM_OR_SIGN':
-                                SYMPTOM_OR_SIGN.append(umls_concept_dict[link['id']])
-                            elif ent['type'] == 'TREATMENT_NAME':
-                                TREATMENT_NAME.append(umls_concept_dict[link['id']])
-                            elif ent['type'] == 'VARIANT':
-                                VARIANT.append(umls_concept_dict[link['id']])
-                            break  # Once we've found the UMLS link, we don't need to go through the rest.  
-                            # TODO: remove this break, as per Liam
+                            if link["id"] in umls_concept_dict:
+                                if ent['type'] == 'BODY_STRUCTURE':
+                                    BODY_STRUCTURE.append(umls_concept_dict[link['id']])
+                                elif ent['type'] == 'CONDITION_QUALIFIER':
+                                    CONDITION_QUALIFIER.append(umls_concept_dict[link['id']])
+                                elif ent['type'] == 'DIAGNOSIS':
+                                    DIAGNOSIS.append(umls_concept_dict[link['id']])
+                                elif ent['type'] == 'DIRECTION':
+                                    DIRECTION.append(umls_concept_dict[link['id']])
+                                elif ent['type'] == 'EXAMINATION_NAME':
+                                    EXAMINATION_NAME.append(umls_concept_dict[link['id']])
+                                elif ent['type'] == 'EXAMINATION_RELATION':
+                                    EXAMINATION_RELATION.append(umls_concept_dict[link['id']])
+                                elif ent['type'] == 'FAMILY_RELATION':
+                                    FAMILY_RELATION.append(umls_concept_dict[link['id']])
+                                elif ent['type'] == 'GENDER':
+                                    GENDER.append(umls_concept_dict[link['id']])
+                                elif ent['type'] == 'GENE':
+                                    GENE.append(umls_concept_dict[link['id']])
+                                elif ent['type'] == 'MEDICATION_CLASS':
+                                    MEDICATION_CLASS.append(umls_concept_dict[link['id']])
+                                elif ent['type'] == 'MEDICATION_NAME':
+                                    MEDICATION_NAME.append(umls_concept_dict[link['id']])
+                                elif ent['type'] == 'ROUTE_OR_MODE':
+                                    ROUTE_OR_MODE.append(umls_concept_dict[link['id']])
+                                elif ent['type'] == 'SYMPTOM_OR_SIGN':
+                                    SYMPTOM_OR_SIGN.append(umls_concept_dict[link['id']])
+                                elif ent['type'] == 'TREATMENT_NAME':
+                                    TREATMENT_NAME.append(umls_concept_dict[link['id']])
+                                elif ent['type'] == 'VARIANT':
+                                    VARIANT.append(umls_concept_dict[link['id']])
+                                break  # Once we've found the UMLS link, we don't need to go through the rest.
+                                # TODO: remove this break, as per Liam
+                            else:
+                                logging.info("Not found in umls_concept_dict: " + link['id'])
+
                 else:
                     logging.info("No links for " + ent["text"])
         
@@ -219,10 +234,12 @@ def transform_value(value, umls_concept_dict):
         logging.info("Entities output: " + json_str)
 
     except:
+        exc_tuple = sys.exc_info()
+        logging.error(str(exc_tuple))
         return (
             {
             "recordId": recordId,
-            "errors": [ { "message": "Could not complete operation for record " + recordId }   ]       
+            "errors": [ { "message": "Failure while processing output for record " + recordId + ".  e: " + str(exc_tuple) }  ]
             })
 
     return ({
